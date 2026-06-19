3f430d62c69303ebfa845bcdf2e60ea26e45c947
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2026-06-19 17:59:35 +0500

    19.06.2026 17:59

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::time::SystemTime;
+use clap::Parser;
+use walkdir::WalkDir;
+use chrono::{DateTime, Utc};
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    #[arg(short, long)]
+    folder: String,
+    #[arg(short, long, default_value_t = 30)]
+    days: u32,
+}
+fn main() {
+    let args = Args::parse();
+    let folder_path = args.folder;
+    let days_limit = args.days;
+    let now = SystemTime::now();
+
+    for entry in WalkDir::new(folder_path).into_iter().filter_map(|e| e.ok()) {
+        let path = entry.path();
+
+        if let Ok(metadata) = entry.metadata() {
+            if let Ok(modified) = metadata.modified() {
+                let duration = now.duration_since(modified).unwrap_or_default();
+                let days_old = (duration.as_secs() / 86400) as u32;
+
+                let datetime: DateTime<Utc> = modified.into();
+                if days_old >= days_limit {
+                    println!("Path: {:?}", path);
+                    println!("Last modified: {}", datetime.format("%Y-%m-%d %H:%M:%S"));
+                    println!("{} day(s) ago", days_old);
+                    println!("---");
+                }
+
+            }
+        }
+    }
+}
